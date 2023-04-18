969e04b0c31de717c8314175cefdd129ad2c865e
Ayokayzy <theayokayzy1@gmail.com>
2023-04-18 15:29:46 +0100

    product category

--- src/context/products-context.jsx
+++ src/context/products-context.jsx
@@ -1,13 +1,23 @@
 import { createContext, useState } from "react";
-import { getAllProducts } from "../utils/apiUtils";
+import {
+  getAllProducts,
+  getCategories,
+  getProductsByCategory,
+} from "../utils/apiUtils";
 
 export const ProductContext = createContext({
   products: [],
   getProducts: () => {},
+  categories: [],
+  getAllCategories: () => {},
+  productMap: [],
+  mapProductToArray: () => {},
 });
 
 export const ProductProvider = ({ children }) => {
-  const [ products, setProducts ] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [productMap, setProductMap] = useState([]);
 
   const getProducts = async () => {
     try {
@@ -18,10 +28,41 @@ export const ProductProvider = ({ children }) => {
     }
   };
 
+  const getAllCategories = async () => {
+    try {
+      const categoriesArray = await getCategories();
+      setCategories(categoriesArray);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  const mapProductToArray = async (categories) => {
+    const productByCat = categories.map(async (category) => {
+      try {
+        const categoryProduct = await getProductsByCategory(category);
+        return { category, products: categoryProduct };
+      } catch (error) {
+        console.log({ error });
+      }
+    });
+    const res = await Promise.all(productByCat);
+    setProductMap(res);
+  };
+
+  // for all categories
+  // fetch category products
+  // map in an array {category: categoryName, products: []}
+
   const value = {
     products,
     getProducts,
+    categories,
+    getAllCategories,
+    mapProductToArray,
+    productMap,
   };
+
   return (
     <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
   );
--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -1,22 +1,30 @@
 import { useContext, useEffect } from "react";
 import ProductCard from "../../components/product-card";
 import { ProductContext } from "../../context/products-context";
-import { getAllProducts } from "../../utils/apiUtils";
+import Category from "../../components/category";
 
 const Shop = () => {
-  const { getProducts, products } = useContext(ProductContext);
+  const { categories, getAllCategories, productMap, mapProductToArray } =
+    useContext(ProductContext);
+    console.log(productMap);
 
   useEffect(() => {
-    getProducts();
+      getAllCategories();
   }, []);
+  
+
+  useEffect(() => {
+    mapProductToArray(categories);
+  }, [categories]);
+
   return (
     <div className="container mx-auto">
       <p className="text-center text-4xl font-medium">Welcome to shop</p>
       <div className="mt-16">
-        <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-8">
-        {products?.map((product) => (
-          <ProductCard price={product.price} image={product.image} title={product.title} />
-        ))}
+        <div className="">
+          {productMap?.map((product) => {
+            return <Category category={product.category} products={product.products} />
+          })}
         </div>
       </div>
     </div>
